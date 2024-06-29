87370c989d36e3b964b9bb30bbb3a3ae6f1d71c4
Natalia Davydova <natasha01013@yandex.ru>
2024-06-29 18:04:25 +0200

    third task

---
+++ js-features/mole-game/task.js
@@ -0,0 +1,32 @@
+//1.3. Игра - убей кротов
+
+let dead = document.getElementById("dead");
+let lost = document.getElementById("lost");
+
+for (let i = 1; i < 10; i++) {
+	let hole = document.getElementById(`hole${index}`);
+
+
+	hole.onclick = function() {
+		if (hole.className.includes('hole_has-mole')) {
+			dead.textContent++;
+		} else {
+			lost.textContent++;
+		}	
+
+		if (dead.textContent >= 10) {
+			alert("Вы победили!");
+			dead.textContent = 0;
+			lost.textContent = 0;
+		} 
+	
+		if (lost.textContent >= 5) {
+			alert("Вы проиграли. Игра окончена.");
+			dead.textContent = 0;
+			lost.textContent = 0;
+		}
+	}
+}
+
+
+
