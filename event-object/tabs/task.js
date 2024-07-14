426c1f991182d90308b18176afa091e674439f17
Natalia Davydova <natasha01013@yandex.ru>
2024-07-14 16:11:53 +0200

    third task with change

--- event-object/tabs/task.js
+++ event-object/tabs/task.js
@@ -8,17 +8,17 @@ let btn = document.querySelector('.tab__navigation');
 //функция, которая будет скрывать вкладки и контент при переключении между ними
 function hideTabContent(x) {
 	for (let i = x; i < contant.length; i++) {
-   	  	contant[i].classList.remove('tab__content_active');
-   	  	navigation[i].classList.remove('tab_active');
-  	}
+		contant[i].classList.remove('tab__content_active');
+		navigation[i].classList.remove('tab_active');
+	}
 }
 
 //функция, которая будет показывать вкладку и контент на ней
 function showTabContent(y) {
 	for (let i = y; i < contant.length; i++) {
-    	contant[y].classList.add('tab__content_active');
-    	navigation[y].classList.add('tab_active');
-  	}
+		contant[y].classList.add('tab__content_active');
+		navigation[y].classList.add('tab_active');
+	}
 }
 
 
@@ -26,11 +26,11 @@ btn.addEventListener('click', function(event) {
 	let target = event.target;  //в переменную target поместим цель объекта - event.target
 		if(target && target.classList.contains('tab')) {  //если клик произошел по родителю вкладок	
 			for(let i = 0; i < navigation.length; i++) {
-  				if (target == navigation[i]) {   //перебираем вкладки tab и сравниваем их с ссылками target
+				if (target == navigation[i]) {   //перебираем вкладки tab и сравниваем их с ссылками target
 				hideTabContent(0);   //если они совпадают, то скроем все вкладки - hideTabContent(0)
-        		showTabContent(i);   //и покажем только одну вкладку, совпадающую по номему в счетчике i
-   	 			break;
-  				}
+				showTabContent(i);   //и покажем только одну вкладку, совпадающую по номему в счетчике i
+				break;
+				}
 			}
 		}
 });
