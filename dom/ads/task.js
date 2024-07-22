addc0f4ce7a6bb7feda00a53261dcdbacb4932e7
Natalia Davydova <natasha01013@yandex.ru>
2024-07-22 13:11:13 +0200

    second task with changes

--- dom/ads/task.js
+++ dom/ads/task.js
@@ -2,23 +2,22 @@
 //Задача 2. Ротатор рекламы
 
 const rotator = Array.from(document.getElementsByClassName('rotator__case'));
-let delay = 1000;
 let i = 0;
 
 const changeStatus = setInterval( () => {
-	rotator[i].classList.remove('rotator__case_active');
-	if (i < rotator.length - 1) {
-		i++;
-	} else {
-		i=0;
-	}
-	rotator[i].classList.add('rotator__case_active');
+	let delay = rotator[i].getAttribute('data-speed');
 
-	let color = rotator[i].getAttribute('data-color');
-	rotator[i].style.color = color;
+	setTimeout( () => {
+		rotator[i].classList.remove('rotator__case_active');
+		if (i < rotator.length - 1) {
+			i++;
+		} else {
+			i=0;
+		}
+		rotator[i].classList.add('rotator__case_active');
 
-//	delay = rotator[i].getAttribute('data-speed');
-//	console.log(delay);
-//	delay = rotator[i].dataset.speed;
-
-}, delay);
+		let color = rotator[i].getAttribute('data-color');
+		rotator[i].style.color = color;
+//		console.log(delay);
+	}, delay);
+}, 1000);
