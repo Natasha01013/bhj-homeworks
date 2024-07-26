1a938754ebad988414dc0892fb7b4ad936b6e65e
Natalia Davydova <natasha01013@yandex.ru>
2024-07-26 11:11:31 +0200

    second task with change

--- dom/ads/task.js
+++ dom/ads/task.js
@@ -1,7 +1,7 @@
 //Домашнее задание к лекции «DOM»
 //Задача 2. Ротатор рекламы
 
-const rotator = Array.from(document.getElementsByClassName('rotator__case'));
+/*const rotator = Array.from(document.getElementsByClassName('rotator__case'));
 let i = 0;
 
 const changeStatus = setInterval( () => {
@@ -20,4 +20,21 @@ const changeStatus = setInterval( () => {
 		rotator[i].style.color = color;
 //		console.log(delay);
 	}, delay);
-}, 1000);
+}, 1000);*/
+
+//Альтернативное решение
+const rotator = Array.from(document.querySelectorAll('.rotator__case'));
+let delay = 1000;
+let i = 0;
+
+function toggleClass() {
+	rotator.forEach((element) => element.classList.remove('rotator__case_active'));
+	rotator[i].classList.add('rotator__case_active');
+	i = rotator[i].nextElementSibling ? i + 1 : 0;
+	delay = rotator[i].getAttribute('data-speed');
+	let color = rotator[i].getAttribute('data-color');
+	rotator[i].setAttribute('style', `color: ${color}`);
+	setTimeout(toggleClass, delay);
+}
+
+setTimeout(toggleClass, delay);
