36a27fcb2d148e9337621bc04a25c8bd462ccbb3
Natalia Davydova <natasha01013@yandex.ru>
2024-08-07 08:46:42 +0200

    first task with changes

--- document-structure/tooltip/task.js
+++ document-structure/tooltip/task.js
@@ -2,26 +2,21 @@
 //Задача 1. Всплывающая подсказка
 
 const hint = document.querySelectorAll('.has-tooltip');
+hint[0].insertAdjacentHTML('afterEnd', '<div class="tooltip"></div>'); //добавляем div в разметку
+const tooltip = document.querySelector('.tooltip');
 
 for (let i = 0; i < hint.length; i++) {
 	hint[i].addEventListener('click', function(event) {
 		event.preventDefault();
 
-//добавляем div в разметку
-		const a = document.getElementsByTagName('a');
-		a[0].insertAdjacentHTML('beforeBegin', '<div class="tooltip"></div>');
-
-		const tooltip = document.querySelector('.tooltip');
-		tooltip.classList.add('tooltip_active');
 		tooltip.innerText = event.target.getAttribute('title');
+		tooltip.classList.add('tooltip_active');
 
-//повышенный уровень сложности - находим координаты подсказок и размещаем их над текстом
-		let position = event.target.getBoundingClientRect();
-//		console.log(position);
+		let position = event.target.getBoundingClientRect(); //находим координаты подсказок, размещаем их над текстом
+		console.log(position);
 
-		let left = position.left + (event.target.offsetWidth - tooltip.offsetWidth) / 2; //посередине подсказки
+		let left = position.left + (event.target.offsetWidth - tooltip.offsetWidth) / 2; //посередине текста
 		if (left < 0) left = 0; // не заезжать за левый край окна
-
 		let top = position.top - tooltip.offsetHeight - 5; //подсказка сверху
 		if (top < 0) { // если подсказка не помещается сверху, то отображать её снизу
 			top = position.top + event.target.offsetHeight + 5;
