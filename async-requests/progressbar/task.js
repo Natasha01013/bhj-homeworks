9835bcb62dbce879ea3e7f7ce797da2d05e631b0
Natasha01013 <natasha01013@yandex.ru>
2024-08-23 10:16:51 +0200

    third task with changes

--- async-requests/progressbar/task.js
+++ async-requests/progressbar/task.js
@@ -3,13 +3,16 @@
 
 const progress = document.getElementById('progress');
 const form = document.getElementById('form');
+const input = document.getElementById('file');
 
 form.onsubmit = function(e) {
     e.preventDefault();
+//    let input = this.elements.userfile;
+    let file = input.files[0];
 
     let ajax = new XMLHttpRequest();
     ajax.upload.onprogress = function(event) {
-        progress.value = event.loaded;
+        progress.value = event.loaded / event.total; //проверка на код состояния ответа
     }
 
     let formData = new FormData();
@@ -19,11 +22,13 @@ form.onsubmit = function(e) {
     ajax.send(formData);
 
     //результат загрузки файла
-    ajax.onload = ajax.onerror = function() {
-        if (this.status === 200) {
+    ajax.onload = function() {
+        if (ajax.status >= 200 && ajax.status < 300) {
             console.log('Файл успешно загружен');
+            console.log(`Ошибка ${ajax.status}: ${ajax.statusText}`);
         } else {
             console.log('Не удалось загрузить файл');
+            console.log(`Ошибка ${ajax.status}: ${ajax.statusText}`);
         }
     }
 }
