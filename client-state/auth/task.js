4181bdca42aaed9f66c36322212ffd5186ff6295
Natasha01013 <natasha01013@yandex.ru>
2024-08-26 17:58:51 +0200

    third task with changes

--- client-state/auth/task.js
+++ client-state/auth/task.js
@@ -3,35 +3,36 @@
 
 const form = document.getElementById('signin');
 const welcome = document.getElementById('welcome');
-const btn = document.getElementById('signin__btn');
 const welcomeUser = document.getElementById('user_id');
+const signinForm = document.getElementById('signin__form');
 
 
 form.onsubmit = function(e) {
     e.preventDefault();
 
     let xhr = new XMLHttpRequest();
-    let formData = new FormData(document.getElementById('signin__form'));
-
+    let formData = new FormData(signinForm);
     xhr.open('POST', 'https://students.netoservices.ru/nestjs-backend/auth');
-    
-    xhr.onload = () => {
-    let responsePars = JSON.parse(xhr.response);
-    console.log(responsePars);
-    if (xhr.readyState === 4) {
-       let userId = Object.values(responsePars)[1]
-        localStorage.setItem('id', userId);
-        welcomeUser.textContent = userId;
-        welcome.classList.add('welcome_active');
-        form.classList.remove('signin_active');
-    } else {
-        console.log(`Ошибка ${xhr.status}: ${xhr.statusText}`)
-        alert('Неверный логин/пароль');
-    }
-    }
+    xhr.responseType = 'json';
 
+    xhr.onload = () => {
+        if ((xhr.response["success"]) === true) {
+            console.log(xhr.response);
+            let userId = xhr.response["user_id"];
+            localStorage.setItem('id', userId);
+            welcomeUser.textContent = userId;
+            welcome.classList.add('welcome_active');
+            form.classList.remove('signin_active');
+         } else {
+            console.log(`Ошибка ${xhr.status}: ${xhr.statusText}`)
+            alert('Неверный логин/пароль');
+         }
+    }
     xhr.send(formData);
+
+    signinForm.reset(); //очистить форму
 }  
+
 
 window.onload = () => { 
     if (localStorage.getItem('id')) { //если в localStorage имеется id пользователя, то сразу выводим Добро пожаловать
